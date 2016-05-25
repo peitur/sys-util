5a601cc208a8cc5c58308deedd51b3aabdb20284
Peter Bartha <peitur@gmail.com>
2016-05-25 21:02:01 +0200

    Corrected ProcessInfo list bug and Updated ProcessUtil with re and exac match searching and added a process_running function to get bool search for a speciffic process name

--- lib/proc/Process.py
+++ lib/proc/Process.py
@@ -82,7 +82,7 @@ class ProcessInfo:
 
             self.__info = self.parse_content( data )
 
-            return self.__info
+            return self
 
         except Exception as error:
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -142,25 +142,47 @@ class ProcessUtil:
     def proc_tree( self, proclist ):
         pass
 
-    def proc_by( self, key, filter, **options ):
+    def proc_by( self, key, objfilter, **options ):
         '''
             Get all processes by name
         '''
-
-        plist = self.scan_proclist()
-
-        for p in plist:
-            k = p.get_by( key )
-            if k == key:
-                result_data.append( p.get_info() )
-
         result_data = []
+
+        match = 'exact'
+        if 'match' in options and options['match'] in ('re','exact'):
+            match = options['match']
+
+        for p in self.scan_proclist():
+            k = p.get_by( key )
+
+            if match == 're':
+                if re.match( objfilter, k ):
+                    result_data.append( p.get_info() )
+            elif match == 'exact':
+                if objfilter == k:
+                    result_data.append( p.get_info() )
+            else:
+                raise RuntimeError("Bad match option : %(r)s" % {'r': match } )
 
         return result_data
 
+    def proc_running( self, pname, **options ):
+
+        options['match'] = 'exact'
+        plist = self.proc_by( 'name', pname, **options )
+        if len( plist ) > 0:
+            return True
+        return False
+
 if __name__ == "__main__":
 
-
-
-    pi = ProcessUtil( debug=True )
+    pi = ProcessUtil( debug=False )
     pprint( pi.scan_proclist() )
+    pl1 = pi.proc_by('name', 'ssh', match='re')
+    print("Got %(n)s 'ssh' re matches " % {'n': len( pl1 ) } )
+    pl2 = pi.proc_by('name', 'ssh', match='exact')
+    print("Got %(n)s 'ssh' exact matches " % {'n': len( pl2 ) } )
+    pl3 = pi.proc_by('name', 'sshd', match='exact')
+    print("Got %(n)s 'sshd' exact matches " % {'n': len( pl3 ) } )
+    print( "Running? 'ssh': %(r)s" % { 'r': pi.proc_running( 'nfs' ) } )
+    print( "Running? 'sshd': %(r)s" % { 'r': pi.proc_running( 'nfsd' ) } )
