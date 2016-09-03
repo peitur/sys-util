15f3b31ff85c70cce7763ef70c84ae51ef0c96bf
peitur <peitur@gmail.com>
2016-09-04 00:39:11 +0200

    a bit more stuff, commands running and abort signal handler added with some basic state handling

--- bin/pingster.py
+++ bin/pingster.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
 
-import os,re,sys
+import os,re,sys,signal
 import json, time
 import threading, queue
-
+import shlex, subprocess
 from pprint import pprint
+
+
+#########################################################
 
 DEFAULT_PING_INTERVAL = 10
 DEFAULT_PING_TIMEOUT = 10
 DEFAULT_PING_CMD = "/usr/bin/ping"
 
+DEFAULT_START_STATE = False
+
 ## 64 bytes from plain1.example.redbridge.se (127.0.0.1): icmp_seq=1 ttl=64 time=0.039 ms
 PING_OK = r"^[0-9]+\s+bytes.+time=[0-9\.]+\s+ms"
 
-
+#########################################################
 stdoutQueue = queue.Queue()
 stderrQueue = queue.Queue()
 timerQueue  = queue.Queue()
 stateQueue  = queue.Queue()
 actionQueue  = queue.Queue()
-
+#########################################################
 
 threadsRunning = True
 
+stateStore = {}
+
+
+def signal_int_handler(signal, frame):
+    global threadsRunning
+    print('You pressed Ctrl+C!')
+    threadsRunning = False
+
+signal.signal( signal.SIGINT, signal_int_handler )
+
+
+#########################################################
 def is_boolean( data ):
     if data in (True, False):
         return True
@@ -86,8 +103,11 @@ def queue_sizes():
         "timerQueue":  timerQueue.qsize()
     }
 
-def ping_parser( line ):
-    pass
+def ping_ok_parser( line ):
+
+    if re.match( PING_OK, line ):
+        return True
+    return False
 
 def ping_thread( options ):
     global threadsRunning
@@ -102,14 +122,21 @@ def ping_thread( options ):
     if "interval" in options: interval = options['interval']
 
     cmd_arr = [ DEFAULT_PING_CMD ]
-    cmd_arr.append( "-i "+interval.__str__() )
+#    cmd_arr.append( "-i "+interval.__str__() )
     cmd_arr.append( hostname )
 
     if debug: stdoutQueue.put( "DEBUG: COMMAND: '%(cmd)s'" % {'cmd': " ".join( cmd_arr ) } )
+
+    proc = subprocess.Popen( cmd_arr, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=128 )
     while threadsRunning:
 
+        line = proc.stdout.readline().lstrip().rstrip()
+        if len( line ) > 0:
+            if  ping_ok_parser( line ):
+                stateQueue.put( { "hostname": hostname, "state": True, "config": options } )
+            else:
+                stateQueue.put( { "hostname": hostname, "state": False , "config": options } )
 
-        pass
 
 
 def action_thread( options ):
@@ -152,10 +179,23 @@ def state_thread( options ):
     debug = False
     if "debug" in options: debug = options['debug']
 
+    global stateStore
     while threadsRunning:
-
         try:
             data = stateQueue.get( block=True, timeout=1)
+
+            hostname = data['hostname']
+            newState = data['state']
+            oldState = stateStore[ hostname ]['state']
+
+            stateStore[ hostname ]['state'] = newState
+
+            if oldState == newState:
+                pass
+            else:
+                stdoutQueue.put( { "hostname":hostname, "from_state": oldState, "to_state": newState, "config": data['config'] } )
+
+
 
         except queue.Empty:
             pass
@@ -204,12 +244,10 @@ def print_stderr_thread( options ):
 if __name__ == "__main__":
 
     options = {}
-    options['debug'] = True
+    options['debug'] = False
 
     options['script'] = sys.argv.pop(0)
     options['filename'] = None
-
-
 
     if len( sys.argv ) > 0:
 
@@ -226,9 +264,17 @@ if __name__ == "__main__":
         try:
 
             config = read_json( options['filename'] )
-            for c in config:
-                kname = "ping_%s" % ( c['hostname'] )
+
+            hostlist = config
+            if 'hostlist' in config:
+                hostlist = config['hostlist']
+
+            for c in hostlist:
+
                 c['debug'] = options['debug']
+                stateStore[ c['hostname'] ] = { "hostname": c['hostname'], "state": DEFAULT_START_STATE, "config": c }
+
+                kname = "ping_%s" % ( c['hostname'] )
                 threads[ kname ] = threading.Thread( target=ping_thread, args=( c ,) )
                 threads[ kname ].start()
 
@@ -238,7 +284,11 @@ if __name__ == "__main__":
         except Exception as error:
             pprint( error )
 
+
+        time.sleep(30)
         threadsRunning = False
+
+        pprint( stateStore )
 
         for t in threads: threads[t].join()
 
